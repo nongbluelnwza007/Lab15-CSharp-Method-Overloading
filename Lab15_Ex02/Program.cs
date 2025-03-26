4872bd39410944b9bdbb50ee2a36fd766431138c
nongbluelnwza007 <booblueboyy@gmail.com>
2025-03-27 00:19:33 +0700

    add

---
+++ Lab15_Ex02/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        WriteLine(Calculator.AddValues(2, 3));         // int + int
+        WriteLine(Calculator.AddValues(2.0f, 3.0f));   // float + float
+        WriteLine(Calculator.AddValues(2L, 3L));       // long + long
+    }
+}
+
+public static class Calculator
+{
+    // Method to add two integers
+    public static int AddValues(int a, int b)
+    {
+        WriteLine("Calculated by method: public static int AddValues(int a, int b)");
+        return a + b;
+    }
+
+    // Method to add two floats
+    public static float AddValues(float a, float b)
+    {
+        WriteLine("Calculated by method: public static float AddValues(float a, float b)");
+        return a + b;
+    }
+
+    // Method to add two longs
+    public static long AddValues(long a, long b)
+    {
+        WriteLine("Calculated by method: public static long AddValues(long a, long b)");
+        return a + b;
+    }
+}
---
+++ Lab15_Ex07/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Derived_2 d2 = new Derived_2();
+        Base b = (Base) d2;
+        Derived_1 d1 = (Derived_1) d2;
+
+        b.A(); // Calls Base.A() or Derived_1.A() depending on the object type
+        d1.A(); // Calls Derived_1.A()
+        d2.A(); // Calls Derived_2.A()
+    }
+}
+
+class Base
+{
+    public virtual void A()
+    {
+        Console.WriteLine("Base.A()");
+    }
+}
+
+class Derived_1 : Base
+{
+    public override void A() // Removed the 'sealed' modifier
+    {
+        Console.WriteLine("Derived_1.A()");
+    }
+}
+
+class Derived_2 : Derived_1
+{
+    public override void A()  // Now this is valid since Derived_1.A() is not sealed anymore
+    {
+        Console.WriteLine("Derived_2.A()");
+    }
+}
